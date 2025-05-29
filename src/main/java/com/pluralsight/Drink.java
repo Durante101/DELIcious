8ba9719e4770581534bbd8a5839f0c0f9140ae0a
Durante101 <durthornton@gmail.com>
2025-05-28 23:02:20 -0400

    Implementing drink class with FoodItem a fixed set of size values inside override getPrice method with switch case and returning price

--- src/main/java/com/pluralsight/Drink.java
+++ src/main/java/com/pluralsight/Drink.java
@@ -1,3 +1,49 @@
+package com.pluralsight;
+
+public class Drink implements FoodItem {
+    private String flavor;
+    private Size size;
+
+    public Drink(String flavor, Size size) {
+        this.flavor = flavor;
+        this.size = size;
+    }
+
+    public String getFlavor() {
+        return flavor;
+    }
+
+    public void setFlavor(String flavor) {
+        this.flavor = flavor;
+    }
+
+    public Size getSize() {
+        return size;
+    }
+
+    public void setSize(Size size) {
+        this.size = size;
+    }
+
+    @Override
+    public String toString() {
+        return "Drink{" +
+                "flavor='" + flavor + '\'' +
+                ", size=" + size +
+                '}';
+    }
+
+
+    // get price of drink based on size
+    @Override
+    public double getPrice() {
+
+        return switch (this.size) {
+            case SMALL -> 2;
+            case MEDIUM -> 2.50;
+            case LARGE -> 3;
+        };
+    }
 
 
 
