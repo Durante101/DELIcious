0c2e8793e9c308be232ec13f9a17b6769efad440
Durante101 <durthornton@gmail.com>
2025-05-29 01:43:19 -0400

    add user prompt and switch case for drink flavor selection

--- src/main/java/com/pluralsight/Userinerface.java
+++ src/main/java/com/pluralsight/Userinerface.java
@@ -47,7 +47,40 @@ public class Userinerface {
 
 
 
+        //the while loop for the next question in this menu would start
+        boolean isSelectingFlavor = true;
 
+        while (isSelectingFlavor) {
+
+            System.out.println("What flavor drink?");
+            System.out.println("""
+                    1) Coke
+                    2) Sprite
+                    3) Apple Juice
+                    """);
+
+            selection = scanner.nextLine();
+            switch (selection) {
+                case "1":
+                    flavor = "Coke";
+                    isSelectingFlavor = false;
+                    break;
+                case "2":
+                    flavor = "Sprite";
+                    isSelectingFlavor = false;
+                    break;
+                case "3":
+                    flavor = "Apple Juice";
+                    isSelectingFlavor = false;
+                    break;
+                default:
+                    System.out.println("Error: Please try again");
+
+            }
+        }
+
+
+        return new Drink(flavor, size);
     }
 
 }
